92eb5e82e7d506ce05287613f39bb0003e70253d
Eric Siegel <eric@locationlabs.com>
2013-05-02 17:36:04 -0700

    Use shell to launch logstash

--- debian/logstash.sh
+++ debian/logstash.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/java -Xmx32m -Djava.io.tmpdir=/var/lib/logstash/ -jar /usr/share/logstash/logstash.jar agent -f /etc/logstash/conf.d --log /var/log/logstash/logstash.log
+exec /usr/bin/java -Xmx32m -Djava.io.tmpdir=/var/lib/logstash/ -jar /usr/share/logstash/logstash.jar agent -f /etc/logstash/conf.d --log /var/log/logstash/logstash.log
