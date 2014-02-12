e014a9a1c6f967125c286b39602770239c17a139
Luis Morales <luis.morales@locationlabs.com>
2014-02-12 23:55:13 +0100

    typo corrected

--- pkg/logstash.sh
+++ pkg/logstash.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/bin/java -Xmx128m -Djava.io.tmpdir=/var/lib/logstash/ -jar /usr/share/logstash/logstash.jar agent -f /etc/logstash/conf.d --log /var/log/logstash/logstash.log
+exec /usr/bin/java -Xmx256m -Djava.io.tmpdir=/var/lib/logstash/ -jar /opt/logstash/logstash.jar agent -f /etc/logstash/conf.d --log /var/log/logstash/logstash.log
